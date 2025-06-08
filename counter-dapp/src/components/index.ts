775047e5302b2e12cb6b64931bcd46d35a798c03
itu-itis21-gizaw21 <gizaw21@itu.edu.tr>
2025-06-08 22:46:34 +0300

    only cli

--- counter-dapp/src/components/index.ts
+++
@@ -1,6 +0,0 @@
-export { Header } from './Header';
-export { Counter } from './Counter';
-export { Footer } from './Footer';
-export { ContractInfo } from './ContractInfo';
-export { StatusIndicator } from './StatusIndicator';
-export { MidnightWalletProvider, useMidnightWallet } from './MidnightWallet';
